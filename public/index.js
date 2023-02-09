a6e212c4a8488c4271362c14107a28ab61d7ba01
Davran04 <117973291+Davran04@users.noreply.github.com>
2023-02-09 12:01:11 +0100

    Delete public directory

--- public/index.js
+++
@@ -1,7 +0,0 @@
-var date = new Date();
-var current_date = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+ date.getDate();
-document.getElementById("p1").innerHTML = current_date;
-
-var date = new Date();
-var current_time = date.getHours()+":"+date.getMinutes()+":"+ date.getSeconds();
-document.getElementById("p2").innerHTML = current_time;
